cb4e1598c5b3dddf8a0ebaa75924ef91be88b157
maraima <49958571+maraima@users.noreply.github.com>
2019-04-24 18:42:33 -0300

    Atualizando com minha classe e arquivo

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://s3-sa-east-1.amazonaws.com/amazons3meurango/fastai/export.pkl'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Bulbasaur', 'Squirtle', 'Charmander', 'Pikachu']
 path = Path(__file__).parent
 
 app = Starlette()
